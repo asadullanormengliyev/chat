0918548e2af6ad8fabdd92ff56974a359a746b9e
Asadulla <normengliyevasadulla@gmail.com>
2025-08-27 10:25:01 +0500

    Add

--- src/main/kotlin/uz/zero_one/chat/Components.kt
+++ src/main/kotlin/uz/zero_one/chat/Components.kt
@@ -18,11 +18,11 @@ class WebMvcConfigure : WebMvcConfigurer{
         setFallbackToSystemLocale(false)
     }
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOriginPatterns("https://sage-sunburst-60ba08.netlify.app","https://chat-h80l.onrender.com","https://f56ace8525b4.ngrok-free.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-    }
+//    override fun addCorsMappings(registry: CorsRegistry) {
+//        registry.addMapping("/**")
+//            .allowedOriginPatterns("https://sage-sunburst-60ba08.netlify.app","https://chat-h80l.onrender.com","https://f56ace8525b4.ngrok-free.app")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .allowedHeaders("*")
+//            .allowCredentials(true)
+//    }
 }
